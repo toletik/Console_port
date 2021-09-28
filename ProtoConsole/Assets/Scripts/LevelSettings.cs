8f3b4cf6c973c60a2de2f002e24652e480841cf6
Joéva Fombaron <joeva.fombaron@gmail.com>
2021-09-28 12:16:09 +0200

    WIP fix player move

--- ProtoConsole/Assets/Scripts/LevelSettings.cs
+++ ProtoConsole/Assets/Scripts/LevelSettings.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class LevelSettings : ScriptableObject
 {
     [SerializeField] private Vector3 gravityCenter = Vector3.zero;
+    [SerializeField] private float planetRadius = 15;
 
     public Vector3 GravityCenter => gravityCenter;
+    public float PlanetRadius => planetRadius;
 }
