5a073498e5dc83928256d04539ae369f626588e7
Léonard <l.fetis@student.isartdigital.com>
2021-10-07 16:57:36 +0200

    Add-detection du player

---
+++ ProtoConsole/Assets/Scripts/Gameplay/Obstacles/TriggerPlayer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private const string PLAYER_TAG = "Player";
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(PLAYER_TAG)) other.GetComponent<Player>().Die();
+    }
+}
