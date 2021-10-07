ce51151dee21c16a177dc6799a01777f3e2e1d2b
Julian Cathelinais Msi <juliancatperso@gmail.com>
2021-10-07 18:07:53 +0200

    Sphere Rotation Wip Method DoActionRotate is empty

--- ProtoConsole/Assets/Scripts/Sphere/Sphere.cs
+++ ProtoConsole/Assets/Scripts/Sphere/Sphere.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +8,27 @@ public class Sphere : MonoBehaviour
     [SerializeField] private SliceEnum typeOfSphere = default;
     private List<GameObject> sphereEntityUncuted = new List<GameObject>();
     private int cutLeft = default;
+    private Action doAction = default;
     void Start()
     {
         sphereEntityUncuted.Add(gameObject);
         if(typeOfSphere == SliceEnum.Half)cutLeft=1;
         else cutLeft=2;
+        doAction = DoActionVoid;
     }
 
-    public void CutTheSphere(){
+	private void Update() {
+		doAction();
+	}
+
+    private void DoActionRotate(){
+
+    }
+
+    private void DoActionVoid(){
+    }
+
+	public void CutTheSphere(){
         int numberOfChildren;
         int numberOfObjectInList;
         List<GameObject> arrayOfGameObject = sphereEntityUncuted;
@@ -28,9 +42,26 @@ public class Sphere : MonoBehaviour
 				}
 			}
         }
+        if(typeOfSphere==SliceEnum.Half&& cutLeft==0)setModeRotate();
+        else if (typeOfSphere==SliceEnum.Quarter&& cutLeft==1)setModeVoid();
     }
 
-    public SliceEnum GetTypeOfSphere(){
+    private void setModeRotate(){
+        doAction = DoActionRotate;
+    }
+
+    private void setModeVoid(){
+        if(typeOfSphere==SliceEnum.Quarter && cutLeft==0)ResetRotation();
+        doAction= DoActionVoid;
+    }
+
+	private void ResetRotation() {
+		for (int i = 0; i < sphereEntityUncuted.Count; i++) {
+            sphereEntityUncuted[i].transform.rotation.eulerAngles.Set(0,0,0);
+		}
+	}
+
+	public SliceEnum GetTypeOfSphere(){
         return typeOfSphere;
     }
 }
