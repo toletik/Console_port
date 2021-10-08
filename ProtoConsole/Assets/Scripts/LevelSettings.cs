253ab6880f4d1eea28efe738f50079c2dc693140
Joéva Fombaron <j.fombaron@student.isartdigital.com>
2021-10-08 12:58:55 +0200

    Add planet radius modifier for planet movement

--- ProtoConsole/Assets/Scripts/LevelSettings.cs
+++ ProtoConsole/Assets/Scripts/LevelSettings.cs
@@ -6,6 +6,13 @@ public class LevelSettings : ScriptableObject
     [SerializeField] private Vector3 gravityCenter = Vector3.zero;
     [SerializeField] private float planetRadius = 15;
 
+    private float movingPlanetRadiusOffset = 0;
+
     public Vector3 GravityCenter => gravityCenter;
-    public float PlanetRadius => planetRadius;
+    public float PlanetRadius => planetRadius + movingPlanetRadiusOffset;
+
+    public void SetPlanetMovingRadiusOffset(float distance)
+    {
+        movingPlanetRadiusOffset = distance;
+    }
 }
