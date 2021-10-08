9542278c6d1aba26b67741ac32f2beffd2cf1980
Léonard <l.fetis@student.isartdigital.com>
2021-10-08 11:30:13 +0200

    Modify - color of Obtacles add - killzone

--- ProtoConsole/Assets/Scripts/Gameplay/Obstacles/KillZone.cs
+++ ProtoConsole/Assets/Scripts/Gameplay/Obstacles/KillZone.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
-public class TriggerPlayer : MonoBehaviour
+public class KillZone : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private float ejectForce = 0.5f;
     private const string PLAYER_TAG = "Player";
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag(PLAYER_TAG)) other.GetComponent<Player>().Eject(-transform.up,ejectForce) ;
+        if (other.CompareTag(PLAYER_TAG)) other.GetComponent<Player>().Die();
     }
 }
