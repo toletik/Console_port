740dd9a6b084167aea98fc7d7d76045feaefc9ab
Léonard <l.fetis@student.isartdigital.com>
2021-10-20 17:33:28 +0200

    Add - CollectibleManager

--- ProtoConsole/Assets/Scripts/Gameplay/Collectible/Collectible.cs
+++ ProtoConsole/Assets/Scripts/Gameplay/Collectible/Collectible.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    public delegate void CollectibleEventHandler(Collectible collectible);
+    static public event CollectibleEventHandler OnCollect;
+    private List<Collectible> list = new List<Collectible>();
+
+    private void Start()
+    {
+        list = CollectibleManager.collectibles;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out Player player))
@@ -9,6 +18,7 @@ public class Collectible : MonoBehaviour
             Debug.Log("Capacity taken !");
 
             player.CollectCapacityToAssign();
+            OnCollect?.Invoke(this);
             Destroy(gameObject);
         }
         else Debug.Log("Invalid recuperation");
--- ProtoConsole/Assets/Scripts/Gameplay/Collectible/CollectibleManager.cs
+++ ProtoConsole/Assets/Scripts/Gameplay/Collectible/CollectibleManager.cs
@@ -5,14 +5,51 @@ using UnityEngine;
 public class CollectibleManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private Transform planetOrigin = default;
+    [SerializeField] private Collectible prefabCollectible = default;
+
+    [SerializeField] private int startCollectible = 0;
+    [SerializeField] private int minNbrOfCollectible = 2;
+
+    static public List<Collectible> collectibles = new List<Collectible>();
     void Start()
     {
-        
+        SpawnCollectibleRandomlyOnSphere(startCollectible);
+        Collectible.OnCollect += Collectible_OnCollect;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Collectible_OnCollect(Collectible collectible)
     {
-        
+        collectibles.RemoveAt(collectibles.IndexOf(collectible));
+        CheckCollectibles();
     }
+
+    private void CheckCollectibles()
+    {
+        int currentCollectible = collectibles.Count;
+
+        if(currentCollectible <= minNbrOfCollectible) SpawnCollectibleRandomlyOnSphere(1);
+    }
+
+    private void SpawnCollectibleRandomlyOnSphere(int nbrCollectible)
+    {
+        Vector3 newPos = default;
+      
+        for (int i = 0; i < nbrCollectible; i++)
+        {
+            newPos = Random.onUnitSphere * 13;
+            newPos.y = Mathf.Abs(newPos.y);
+      
+            Collectible collectible = Instantiate(prefabCollectible, newPos, Quaternion.identity);
+            collectible.transform.rotation = Quaternion.AngleAxis(Vector3.Angle(planetOrigin.up, newPos), Vector3.Cross( planetOrigin.up - planetOrigin.position,newPos - planetOrigin.position)) * collectible.transform.rotation;
+            collectibles.Add(collectible);
+        }        
+    }
+    private void OnDestroy()
+    {
+        Collectible.OnCollect -= Collectible_OnCollect;
+    }
+
+
+
 }
