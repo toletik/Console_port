7c0af2cda33131742c18479f6f39d24292b0b9ad
Léonard <l.fetis@student.isartdigital.com>
2021-10-26 12:44:24 +0200

    Change - Collectible Manager (Collectible place with camera)

--- ProtoConsole/Assets/Scripts/Gameplay/Collectible/CollectibleManager.cs
+++ ProtoConsole/Assets/Scripts/Gameplay/Collectible/CollectibleManager.cs
@@ -11,10 +11,16 @@ public class CollectibleManager : MonoBehaviour
     [SerializeField] private int startCollectible = 0;
     [SerializeField] private int minNbrOfCollectible = 2;
 
+    [SerializeField] private float radiusOnDeath = 5;
+    [SerializeField] private Camera cameraTest = default;
+
+
     static public List<Collectible> collectibles = new List<Collectible>();
     void Start()
     {
-        SpawnCollectibleRandomlyOnSphere(startCollectible);
+       
+        //LoseCollectibleWhenDead(debugTransform, 2);
+        SpawnCollectibleRandomlyOnSphere(100);
         Collectible.OnCollect += Collectible_OnCollect;
     }
 
@@ -39,11 +45,48 @@ public class CollectibleManager : MonoBehaviour
         {
             newPos = Random.onUnitSphere * 13;
             newPos.y = Mathf.Abs(newPos.y);
-      
+            newPos =   Quaternion.FromToRotation(Vector3.up, -cameraTest.transform.forward) *(newPos - Vector3.zero);
+
+
+
             Collectible collectible = Instantiate(prefabCollectible, newPos, Quaternion.identity);
             collectible.transform.rotation = Quaternion.AngleAxis(Vector3.Angle(planetOrigin.up, newPos), Vector3.Cross( planetOrigin.up - planetOrigin.position,newPos - planetOrigin.position)) * collectible.transform.rotation;
             collectibles.Add(collectible);
         }        
+    }
+    private void LoseCollectibleWhenDead(Transform playerTransform,int nbrOfCollectible)
+    {
+        float angle;
+        float ratio;
+        Vector3 newPos = default;
+
+        for (int i = 0; i < nbrOfCollectible; i++)
+        {
+            ratio = (float)i / nbrOfCollectible;
+
+            angle = 2 * Mathf.PI * ratio;
+
+
+
+            newPos  = new Vector3(radiusOnDeath * Mathf.Cos(angle),
+                0,
+                radiusOnDeath * Mathf.Sin(angle))+playerTransform.position;
+
+            CreateCollectible(prefabCollectible, newPos);
+
+
+        }
+
+    }
+    private void CreateCollectible(Collectible prefab,Vector3 newPos)
+    {
+        Collectible collectible = Instantiate(prefab, newPos, Quaternion.identity);
+        collectibles.Add(collectible);
+    }
+    private void CreateCollectible(Collectible prefab, Vector3 newPos,Transform parent)
+    {
+        Collectible collectible = Instantiate(prefab, newPos, Quaternion.identity,parent);
+        collectibles.Add(collectible);
     }
     private void OnDestroy()
     {
--- ProtoConsole/Assets/Scripts/Gameplay/Obstacles/KillZone.cs
+++ ProtoConsole/Assets/Scripts/Gameplay/Obstacles/KillZone.cs
@@ -6,7 +6,13 @@ using UnityEngine;
 public class KillZone : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private Camera cameraTest = default;
     private const string PLAYER_TAG = "Player";
+
+    private void Start()
+    {
+        transform.position = Quaternion.FromToRotation(Vector3.up, -cameraTest.transform.forward) * (transform.position - Vector3.zero);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(PLAYER_TAG)) other.GetComponent<Player>().Die();
