104d5396c4b86e76db721553c6e79722421a73f8
Julian Cathelinais Msi <juliancatperso@gmail.com>
2021-11-04 12:52:11 +0100

    Update Title Card

--- ProtoConsole/Assets/Scripts/UI/TitleCard.cs
+++ ProtoConsole/Assets/Scripts/UI/TitleCard.cs
@@ -1,9 +1,32 @@
 using Com.IsartDigital.Common.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TitleCard : UIScreen
 {
+	[SerializeField] GameObject selectButtonInSettings = default;
+	private bool vibrationEnabled = default;
 	public void OnPlayButtonClick() 
     {
         UIManager.Instance.AddScreen<ConnexionScreen>(true);
+	}
+
+	public void UpdateSelectButton(GameObject button) {
+		EventSystem.current.SetSelectedGameObject(null);
+		EventSystem.current.SetSelectedGameObject(button);
+	}
+
+	public void SwitchVibration()
+	{
+		if(vibrationEnabled)
+		{
+			vibrationEnabled=false;
+			VibrationManager.GetSingleton().enableVibrations = false;
+		}
+		else
+		{
+			vibrationEnabled=false;
+			VibrationManager.GetSingleton().enableVibrations = true;
+		}
 	}
 }
